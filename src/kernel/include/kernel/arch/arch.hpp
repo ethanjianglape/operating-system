322ee73a75a9cec360d9abd40824f6310d9688ab
Ethan Jiang-Lape <ethanjianglape@gmail.com>
2025-12-15 13:24:54 -0500

    remove clang tidy warnings

--- src/kernel/include/kernel/arch/arch.hpp
+++ src/kernel/include/kernel/arch/arch.hpp
@@ -3,7 +3,8 @@
 #include <arch/i686/vmm/vmm.hpp>
 #include <arch/i686/cpu/cpu.hpp>
 
+// Allows kernel library code to indirectly access the current CPU architecture
 namespace kernel::arch {
     namespace vmm = ::i686::vmm;
-    namespace cpu    = ::i686::cpu;
+    namespace cpu = ::i686::cpu;
 }
